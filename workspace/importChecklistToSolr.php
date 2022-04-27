51f5a025fcc3c5927cc96cb61f05b77ec2dbb015
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-04-27 15:05:36 +0800

    update to incorporate taxon higher than species

--- workspace/importChecklistToSolr.php
+++ workspace/importChecklistToSolr.php
@@ -49,21 +49,39 @@ $counter = 0;
 $ret = array();
 
 $file = (empty($argv[1]))?"":$argv[1];
+// $file = '/code/source-data/source_taicol_20220427_test.csv';
 $fp = fopen($file, "r");
 
 $source = (empty($argv[2]))?basename($file, '.csv'):$argv[2];
 
+// kim: tab-separated
 while ($vals = fgetcsv($fp, 0, "\t" )) {
 	$vals = array_map("trim", $vals, array_fill(0, count($vals), "\r\n\t ,.'\""));
 
+	// kim: 如果本身沒有name_id的話，建一個hash
 	if (empty($vals[0]) && empty($vals[1])) {
-		$vals[0] = "sci_hash_" . md5($vals[2]);
+		$vals[0] = "sci_hash_" . md5($vals[2]); 
 		$vals[1] = $vals[0];
 	}
 	else if (empty($vals[0])) {
 		$vals[0] = $vals[1] . '-s5-v' . md5($vals[2]);
 	}
 
+	/**
+	 * 0 namecode
+	 * 1 accepted_namecode
+	 * 2 scientific_name
+	 * 3 name_url_id
+	 * 4 accepted_url_id
+	 * 5 common_name_c
+	 * 6 taxon_rank
+	 * 7 genus
+	 * 8 family
+	 * 9 order
+	 * 10 class
+	 * 11 phylum
+	 * 12 kingdom
+	 */
 
 	$rec = array();
 	$rec['id'] = $source . '-' . $vals[0];
@@ -77,18 +95,25 @@ while ($vals = fgetcsv($fp, 0, "\t" )) {
 		$rec['a_url_id'] = $vals[3];
 	}
 
-	$rec['family'] = array_shift(explode(" ", $vals[5]));
-	$rec['order'] = array_shift(explode(" ", $vals[6]));
-	$rec['class'] = array_shift(explode(" ", $vals[7]));
-	$rec['phylum'] = array_shift(explode(" ", $vals[8]));
-	$rec['kingdom'] = array_shift(explode(" ", $vals[9]));
+	// kim: 只取單字
+	$rec['genus'] = array_shift(explode(" ", $vals[7]));
+	$rec['family'] = array_shift(explode(" ", $vals[8]));
+	$rec['order'] = array_shift(explode(" ", $vals[9]));
+	$rec['class'] = array_shift(explode(" ", $vals[10]));
+	$rec['phylum'] = array_shift(explode(" ", $vals[11]));
+	$rec['kingdom'] = array_shift(explode(" ", $vals[12]));
 
+	$rec['sound_genus'] = treat_word($rec['genus']);
 	$rec['sound_family'] = treat_word($rec['family']);
 	$rec['sound_order'] = treat_word($rec['order']);
 	$rec['sound_class'] = treat_word($rec['class']);
 	$rec['sound_phylum'] = treat_word($rec['phylum']);
 	$rec['sound_kingdom'] = treat_word($rec['kingdom']);
 
+	// kim: 如果高階層和自己同階層就拿掉
+	if (array_key_exists(strtolower($rec['taxon_rank']), $rec)) {
+		$rec[strtolower($rec['taxon_rank'])] = null;
+	}
 
 	$rec['namecode'] = $vals[0];
 //	$rec['taibnet_url'] = "http://taibnet.sinica.edu.tw/chi/taibnet_species_detail.php?name_code=" . $vals[0];
@@ -103,37 +128,42 @@ while ($vals = fgetcsv($fp, 0, "\t" )) {
 	//if ($rec['canonical_name'] == 'Bombyx pernyi') {
 		//var_dump($rec);
 	//}
+	$rec['taxon_rank'] = $vals[6];
+	$rec['common_name_c'] = $vals[5];
+
 	$rec['sound_name'] = treat_word($rec['canonical_name']);
+
 	$frags = explode(" ", $rec['canonical_name']);
-	$rec['latin_part_a'] = $frags[0];
-	$rec['genus'] = $frags[0];
-	$rec['sound_part_a'] = treat_word($frags[0]);
-	$rec['sound_genus'] = $frags[0];
-	$rec['sound_part_a_strip_ending'] = treat_word($frags[0], true);
+	if (count($frags) > 1){
+		$rec['latin_part_a'] = $frags[0];
+		$rec['genus'] = $frags[0];
+		$rec['sound_part_a'] = treat_word($frags[0]);
+		$rec['sound_genus'] = $frags[0];
+		$rec['sound_part_a_strip_ending'] = treat_word($frags[0], true);
 
-	$rec['nameSpell'][] = $frags[0];
+		$rec['nameSpell'][] = $frags[0];
 
-	if (!empty($frags[1])) {
-		$rec['latin_part_bc'][] = $frags[1];
-		$rec['nameSpell'][] = $frags[1];
-		$rec['nameSpell'][] = $frags[0] . " " . $frags[1];
-		$rec['sound_part_bc'][] = treat_word($frags[1]);
-		$rec['sound_part_bc_strip_ending'][] = treat_word($frags[1], true);
-	}
-	else {
-		continue;
-	}
+		if (!empty($frags[1])) {
+			$rec['latin_part_bc'][] = $frags[1];
+			$rec['nameSpell'][] = $frags[1];
+			$rec['nameSpell'][] = $frags[0] . " " . $frags[1];
+			$rec['sound_part_bc'][] = treat_word($frags[1]);
+			$rec['sound_part_bc_strip_ending'][] = treat_word($frags[1], true);
+		}
+		else {
+			continue;
+		}
 
-	if (!empty($frags[2])) {
-		$rec['latin_part_bc'][] = $frags[2];
-		$rec['nameSpell'][] = $frags[2];
-		$rec['nameSpell'][] = $frags[1] . " " . $frags[2];
-		$rec['nameSpell'][] = $frags[0] . " " . $frags[2];
-		$rec['nameSpell'][] = $frags[0] . " " . $frags[1] . " " . $frags[2];
-		$rec['sound_part_bc'][] = treat_word($frags[2]);
-		$rec['sound_part_bc_strip_ending'][] = treat_word($frags[2], true);
+		if (!empty($frags[2])) {
+			$rec['latin_part_bc'][] = $frags[2];
+			$rec['nameSpell'][] = $frags[2];
+			$rec['nameSpell'][] = $frags[1] . " " . $frags[2];
+			$rec['nameSpell'][] = $frags[0] . " " . $frags[2];
+			$rec['nameSpell'][] = $frags[0] . " " . $frags[1] . " " . $frags[2];
+			$rec['sound_part_bc'][] = treat_word($frags[2]);
+			$rec['sound_part_bc_strip_ending'][] = treat_word($frags[2], true);
+		}
 	}
-	
 	$ret[] = $rec;
 
 	if ($counter % 1000 == 999) {
