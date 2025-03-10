b650b7d8ca39f4e9ac03bcb2249a0b3f3be0869b
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2025-03-10 15:36:29 +0800

    update

--- include/queryNames.php
+++ include/queryNames.php
@@ -68,22 +68,22 @@ function queryNameSingle($name, $name_cleaned, $against, $best, $ep, $bio_group,
 	if ($best=='yes'&&!(preg_match("/\p{Han}+/u", $name_cleaned))) { // best, 不是中文
 		// 如果輸入單一個英文字母
 		if (strlen($name_cleaned) == 1 ){
-			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&fq=-source:taicol&rows=0&q=' . rawurlencode($name_cleaned) ;
+			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&rows=0&q=' . rawurlencode($name_cleaned) ;
 		} else {
-			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&fq=-source:taicol&rows=0&q=' . rawurlencode($name_cleaned) .'~1';
+			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&rows=0&q=' . rawurlencode($name_cleaned) .'~1';
 		}
 	} 
 	elseif (preg_match("/\p{Han}+/u", $name_cleaned)) { // 是中文 只回傳異體字相符 只查詢common_name_c有值的資料
 		// 中文只回傳有效名
 		$name_used_query = rawurlencode(treat_word_c($name_cleaned));
-		$ep .= '/select?wt=json&rows=0&fl=*,score&fq=-source:taicol&fq=name_status:accepted&fq=common_name_c:*&q=common_name_c:/' . $name_used_query .  '/^6%20or%20alternative_name_c:/' . $name_used_query . '/^5';
+		$ep .= '/select?wt=json&rows=0&fl=*,score&fq=name_status:accepted&fq=common_name_c:*&q=common_name_c:/' . $name_used_query .  '/^6%20or%20alternative_name_c:/' . $name_used_query . '/^5';
 	}
 	elseif (!preg_match("/\p{Han}+/u", $name_cleaned)) { // 不是best, 不是中文
 
 		if (strlen($name_cleaned) == 1 ){
-			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&fq=-source:taicol&rows=0&q=/.*' . rawurlencode($name_cleaned) . '.*/';
+			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&rows=0&q=/.*' . rawurlencode($name_cleaned) . '.*/';
 		} else {
-			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&fq=-source:taicol&rows=0&q=' . rawurlencode($name_cleaned) .'~';
+			$ep .= '/select?wt=json&fq=is_single_word%3Atrue&rows=0&q=' . rawurlencode($name_cleaned) .'~';
 		}
 	} 
 
@@ -170,7 +170,7 @@ function queryNames ($name, $against, $best, $ep, $bio_group, $is_in_taiwan, $ta
 
 	if (empty($ep)) return false;
 
-	$ep .= '/select?wt=json&q=*:*&fq=-source:taicol&sort=source%20asc'; 
+	$ep .= '/select?wt=json&q=*:*&sort=source%20asc'; 
 
 	if ($against == 'taicol' && isset($is_in_taiwan)){
 		$ep .= '&fq=is_in_taiwan:' . $is_in_taiwan;
